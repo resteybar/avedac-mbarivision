4fb4bd182baf66ac0b4b0e638fd0dd793318a069
Danelle Cline <dcline@mbari.org>
2018-06-19 15:53:34 -0700

    Moved saliency check-out outside Docker image because Docker doesn't support secure password sharing during the build cycle only.

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Missing password argument, e.g. ./build.sh <saliency password>"
+    exit -1 
+fi
+if [ ! -d $PWD/src/saliency ]; then
+    pushd $PWD/src
+    
+    # Suppress warning about storing unencrypted password that prompts yes/no answer
+    mkdir ~/.subversion
+    echo "[global]" > ~/.subversion/config
+    echo "store-plaintext-passwords=off" >> ~/.subversion/config
+
+    # Check out saliency 
+    svn checkout --username anonsvn --password $1 svn://isvn.usc.edu/software/invt/trunk/saliency 
+    popd
+fi
+
+# Build everything
 docker-compose build
